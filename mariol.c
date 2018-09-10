28fc377c214afe49379dfb572c606d26cd83201a
rana3010 <42323978+rana3010@users.noreply.github.com>
2018-09-10 04:27:11 +0200

    Add files via upload

---
+++ mariol.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <cs50.h>
+int main (void)
+{
+    int x=get_int("height: ");
+
+        while(x<0 ||x>23)
+         {
+           x=get_int("height: ");
+         }
+         for(int r=1;r<=x ;r++)
+        {
+            for(int i=0;i<x-r;i++)
+            {
+                printf(" ");
+            }
+            for(int c=0;c<r+1 ;c++)
+            {
+                printf("#");
+            }
+
+            printf("\n");
+        }
+}
